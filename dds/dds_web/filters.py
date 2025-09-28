0576db0524f93fffe8b94c57702d8d116c414438
Vladimir_Filippov <v.filippov1994@gmail.com>
2025-09-28 11:57:54 +0700

    add empty label

--- dds/dds_web/filters.py
+++ dds/dds_web/filters.py
@@ -18,18 +18,22 @@ class RecordFilter(django_filters.FilterSet):
     )
     status = django_filters.ModelChoiceFilter(
         queryset=Status.objects.all(),
+        empty_label='Все',
         widget=forms.Select(attrs={'class':'form-select'})
     )
     type = django_filters.ModelChoiceFilter(
         queryset=Type.objects.all(),
+        empty_label='Все',
         widget=forms.Select(attrs={'class':'form-select'})
     )
     category = django_filters.ModelChoiceFilter(
         queryset=Category.objects.all(),
+        empty_label='Все',
         widget=forms.Select(attrs={'class':'form-select'})
     )
     subcategory = django_filters.ModelChoiceFilter(
         queryset=SubCategory.objects.all(),
+        empty_label='Все',
         widget=forms.Select(attrs={'class':'form-select'})
     )
 
